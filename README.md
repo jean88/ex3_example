26a2643c2e1d32891300a7253a338ee9e3021c25
jean88 <jean-francois.robichaud.1@ulaval.ca>
2013-09-21 11:22:44 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Ex3_example
+
+SIO-6014
+
+Jean-Francois Robichaud
---
+++ bin/ex3_example.dart
@@ -0,0 +1,7 @@
+import 'package:ex3/ex3.dart';
+
+main() {
+  ex2_1(10, 20); //this will calculate the area in sq m
+  ex2_2(100.0, 9.58); //this will calculate the speed in km/h between m and time in sec.
+  
+}
